8a9a8d328aacdcb8b31762494f2d8942158cb2a2
mindman79@hotmail.com <LwKmRWUc25hH>
2020-11-19 12:22:59 -0700

    Finished new release

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
 import org.apache.commons.io.FileUtils;
 
@@ -12,24 +13,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.AccessDeniedException;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
 public class Controller {
-
-
-    public static File temp = new File("C:/Temp");
-
-    public static File x64 = new File("C:/CDKRecreation/lsinstall_2.0_x64.exe -q");
-    public static File x86 = new File("C:/CDKRecreation/lsinstall_2.0_x86.exe -q");
-
-    public static File cache = new File("C:/Program Files (x86)/LightspeedEVO Clients");
-    public static File automation1ClientCache = new File(cache + "/LightspeedEVO");
-    public static File automation2ClientCache = new File(cache + "/LightspeedEVO2");
-
-    public static File automation1TempFolder = new File(temp + "/LightspeedEVO");
-    public static File automation2TempFolder = new File(temp + "/LightspeedEVO2");
-
 
     @FXML
     private ResourceBundle resources;
@@ -47,25 +35,66 @@ public class Controller {
     private RadioButton Bottom32bit;
 
     @FXML
+    private TextArea textArea;
+
+    @FXML
     private javafx.scene.control.ProgressBar ProgressBar;
+
+    public static File temp = new File("C:/Temp");
+
+    public static File x64 = new File("C:/CDKRecreation/lsinstall_2.0_x64.exe -q");
+    public static File x86 = new File("C:/CDKRecreation/lsinstall_2.0_x86.exe -q");
+
+    public static File cache = new File("C:/Program Files (x86)/LightspeedEVO Clients");
+    public static File automation1ClientCache = new File(cache + "/LightspeedEVO");
+    public static File automation2ClientCache = new File(cache + "/LightspeedEVO2");
+
+    public static File automation1TempFolder = new File(temp + "/LightspeedEVO");
+    public static File automation2TempFolder = new File(temp + "/LightspeedEVO2");
+
+    public static final String evoRunning = "Oops! Evo is currently running. Try again after closing it.";
+    public static final String evoNotRunning = "Evo is not currently running, great job! Proceeding.";
+    public static final String processAborted = "Process failed. Please try again.";
+
 
     @FXML
     void Bottom32bitButton(ActionEvent event) throws IOException {
 
-        System.out.println("32-bit selected");
+        textArea.clear();
 
-        processChange(x86);
+        try {
+
+            textArea.appendText("32-bit selected." + "\n\n");
+
+            processChange(x86);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            textArea.appendText(e + "\n\n");
+            textArea.appendText(processAborted + "\n\n");
+            Top64bit.setSelected(false);
+            Bottom32bit.setSelected(false);
+        }
 
     }
 
     @FXML
     void Top64bitButton(ActionEvent event) throws IOException {
 
+        textArea.clear();
 
-        System.out.println("64-bit selected");
+        try {
 
+            textArea.appendText("64-bit selected." + "\n\n");
 
-        processChange(x64);
+            processChange(x64);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            textArea.appendText(e + "\n\n");
+            textArea.appendText(processAborted);
+            Top64bit.setSelected(false);
+            Bottom32bit.setSelected(false);
+
+        }
 
     }
 
@@ -74,59 +103,90 @@ public class Controller {
     void initialize() throws IOException {
 
 
-        if (!temp.exists()) {
+        try {
+            if (!temp.exists()) {
 
-            FileUtils.forceMkdir(temp);
+                FileUtils.forceMkdir(temp);
 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            textArea.appendText(e + "\n\n");
+            textArea.appendText(processAborted);
+            Top64bit.setSelected(false);
+            Bottom32bit.setSelected(false);
         }
 
     }
 
     public Boolean checkForRunning() throws IOException {
 
-        boolean running;
+        boolean running = false;
 
-        String line;
-        String pidInfo = "";
+        try {
 
-        Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
+            String line;
+            String pidInfo = "";
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
 
-        while ((line = input.readLine()) != null) {
-            pidInfo += line;
-        }
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-        input.close();
+            while ((line = input.readLine()) != null) {
+                pidInfo += line;
+            }
 
-        if (pidInfo.contains("chrome.exe")) {
+            input.close();
 
-            running = true;
+            if (pidInfo.contains("Lightspeed.exe")) {
 
-        } else {
+                running = true;
 
-            running = false;
+            } else if (pidInfo.contains("Lightspeed_x64.exe")) {
+
+                running = true;
+
+            } else {
+
+                running = false;
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            textArea.appendText(e + "\n\n");
+            textArea.appendText(processAborted);
+            Top64bit.setSelected(false);
+            Bottom32bit.setSelected(false);
+
         }
 
         return running;
     }
 
-    public void processChange(File file) throws IOException {
+
+    public void processChange(File file) throws IOException, InterruptedException {
+
 
         if (checkForRunning()) {
 
-            System.out.println("Chrome is running!");
+            textArea.appendText(evoRunning + "\n\n");
+            Top64bit.setSelected(false);
+            Bottom32bit.setSelected(false);
 
         } else {
 
-            System.out.println("Chrome is not running!");
+            textArea.appendText(evoNotRunning + "\n\n");
 
             new Thread(() -> {
 
                 try {
 
+
                     FileUtils.copyDirectoryToDirectory(automation1ClientCache, temp);
                     FileUtils.copyDirectoryToDirectory(automation2ClientCache, temp);
+
+                    TimeUnit.SECONDS.sleep(3);
 
                     Platform.runLater(() -> ProgressBar.setProgress(0.25));
 
@@ -143,18 +203,25 @@ public class Controller {
                     FileUtils.copyDirectoryToDirectory(automation1TempFolder, cache);
                     FileUtils.copyDirectoryToDirectory(automation2TempFolder, cache);
 
-                    TimeUnit.SECONDS.sleep(3);
+                    TimeUnit.SECONDS.sleep(1);
 
                     Platform.runLater(() -> ProgressBar.setProgress(1));
-                } catch (IOException e) {
+
+                    textArea.appendText("Process complete!" + "\n\n");
+
+                } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    textArea.appendText(e + "\n\n");
+                    textArea.appendText(processAborted);
+                    Top64bit.setSelected(false);
+                    Bottom32bit.setSelected(false);
                     Thread.currentThread().interrupt();
+                    return;
                 }
 
 
             }).start();
+
 
         }
 
